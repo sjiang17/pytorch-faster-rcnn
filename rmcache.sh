a20ba612f3651787ded12b79df99da71591c8d5e
sjiang17 <styjiang@163.com>
2018-04-25 21:35:27 +0000

    added remove cache

--- rmcache.sh
+++ rmcache.sh
@@ -1,2 +1,4 @@
-rm -r /siyuvol/pytorch-faster-rcnn/data/cache
-
+rm -r ./data/cache
+rm -r ./data/VOCdevkit2007/annotations_cache/ 
+rm -r ./data/VOCdevkit2007/results/
+rm ./data/VOCdevkit2007/VOC2007/ImageSets/Main/test.txt_annots.pkl
