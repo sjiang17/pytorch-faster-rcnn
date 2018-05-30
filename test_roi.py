c633ce706bf376008b2bef63889b4ba64bfdea47
sjiang17 <styjiang@163.com>
2018-05-30 10:32:42 -0700

    added test roi

---
+++ test_roi.py
@@ -0,0 +1,247 @@
+# --------------------------------------------------------
+# Tensorflow Faster R-CNN
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Jiasen Lu, Jianwei Yang, based on code from Ross Girshick
+# --------------------------------------------------------
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import _init_paths
+import os
+import sys
+import numpy as np
+import argparse
+import pprint
+import pdb
+import time
+import cv2
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.optim as optim
+import pickle
+from roi_data_layer.roidb import combined_roidb
+from roi_data_layer.roibatchLoader import roibatchLoader
+from model.utils.config import cfg, cfg_from_file, cfg_from_list, get_output_dir
+from model.rpn.bbox_transform import clip_boxes
+from model.nms.nms_wrapper import nms
+from model.rpn.bbox_transform import bbox_transform_inv
+from model.utils.net_utils import save_net, load_net, vis_detections
+from model.faster_rcnn.vgg16 import vgg16
+# from model.faster_rcnn.resnet import resnet
+
+import pdb
+
+try:
+    xrange          # Python 2
+except NameError:
+    xrange = range  # Python 3
+
+
+def parse_args():
+  """
+  Parse input arguments
+  """
+  parser = argparse.ArgumentParser(description='Train a Fast R-CNN network')
+  parser.add_argument('--dataset', dest='dataset',
+                      help='training dataset',
+                      default='kitti', type=str)
+  parser.add_argument('--cfg', dest='cfg_file',
+                      help='optional config file',
+                      default='cfgs/vgg16.yml', type=str)
+  parser.add_argument('--net', dest='net',
+                      help='vgg16, res50, res101, res152',
+                      default='vgg16', type=str)
+  parser.add_argument('--set', dest='set_cfgs',
+                      help='set config keys', default=None,
+                      nargs=argparse.REMAINDER)
+  parser.add_argument('--load_dir', dest='load_dir',
+                      help='directory to load models', default="/siyuvol/pytorch-model",
+                      nargs=argparse.REMAINDER)
+  parser.add_argument('--cuda', dest='cuda',
+                      help='whether use CUDA',
+                      action='store_true')
+  parser.add_argument('--ls', dest='large_scale',
+                      help='whether use large imag scale',
+                      action='store_true')
+  parser.add_argument('--mGPUs', dest='mGPUs',
+                      help='whether use multiple GPUs',
+                      action='store_true')
+  parser.add_argument('--cag', dest='class_agnostic',
+                      help='whether perform class_agnostic bbox regression',
+                      action='store_true')
+  parser.add_argument('--parallel_type', dest='parallel_type',
+                      help='which part of model to parallel, 0: all, 1: model before roi pooling',
+                      default=0, type=int)
+  parser.add_argument('--checksession', dest='checksession',
+                      help='checksession to load model',
+                      default=1, type=int)
+  parser.add_argument('--checkepoch', dest='checkepoch',
+                      help='checkepoch to load network',
+                      default=1, type=int)
+  parser.add_argument('--checkpoint', dest='checkpoint',
+                      help='checkpoint to load network',
+                      default=10021, type=int)
+  parser.add_argument('--bs', dest='batch_size',
+                      help='batch_size',
+                      default=1, type=int)
+  parser.add_argument('--vis', dest='vis',
+                      help='visualization mode',
+                      action='store_true')
+  args = parser.parse_args()
+  return args
+
+lr = cfg.TRAIN.LEARNING_RATE
+momentum = cfg.TRAIN.MOMENTUM
+weight_decay = cfg.TRAIN.WEIGHT_DECAY
+
+if __name__ == '__main__':
+
+  os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+  args = parse_args()
+
+  print('Called with args:')
+  print(args)
+
+  if torch.cuda.is_available() and not args.cuda:
+    print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+
+  np.random.seed(cfg.RNG_SEED)
+  if args.dataset == "kitti":
+      args.imdb_name = "kitti_2007_trainval"
+      args.imdbval_name = "kitti_2007_test"
+      args.set_cfgs = ['ANCHOR_SCALES', '[8, 16, 32]', 'ANCHOR_RATIOS', '[0.5,1,2]']   
+
+  args.cfg_file = "cfgs/{}_ls.yml".format(args.net) if args.large_scale else "cfgs/{}.yml".format(args.net)
+
+  if args.cfg_file is not None:
+    cfg_from_file(args.cfg_file)
+  if args.set_cfgs is not None:
+    cfg_from_list(args.set_cfgs)
+
+  print('Using config:')
+  pprint.pprint(cfg)
+
+  cfg.TRAIN.USE_FLIPPED = False
+  imdb, roidb, ratio_list, ratio_index = combined_roidb(args.imdbval_name, False)
+  imdb.competition_mode(on=True)
+
+  print('{:d} roidb entries'.format(len(roidb)))
+
+  input_dir = args.load_dir + "/" + args.net + "/" + args.dataset
+  # if not os.path.exists(input_dir):
+    # raise Exception('There is no input directory for loading network from ' + input_dir)
+  # load_name = os.path.join(input_dir,
+    #'faster_rcnn_{}_{}_{}.pth'.format(1, 10, 625))
+  # load_name = os.path.join(input_dir, 'faster_rcnn_vgg16_coco-jwy.pth')
+  load_name = '/siyuvol/pytorch-faster-rcnn/model/vgg16/kitti/faster_rcnn_1_19_5983.pth'
+
+  # initilize the network here.
+  if args.net == 'vgg16':
+    fasterRCNN = vgg16(imdb.classes, pretrained=False, class_agnostic=args.class_agnostic)
+  else:
+    print("network is not defined")
+    pdb.set_trace()
+  
+  image_save_dir = '/siyuvol/output/'
+  if not os.path.exists(image_save_dir):
+    os.makedirs(image_save_dir)
+
+  fasterRCNN.create_architecture()
+
+  print("load checkpoint %s" % (load_name))
+  checkpoint = torch.load(load_name)
+  fasterRCNN.load_state_dict(checkpoint['model'])
+  if 'pooling_mode' in checkpoint.keys():
+    cfg.POOLING_MODE = checkpoint['pooling_mode']
+
+
+  print('load model successfully!')
+  # initilize the tensor holder here.
+  im_data = torch.FloatTensor(1)
+  im_info = torch.FloatTensor(1)
+  num_boxes = torch.LongTensor(1)
+  gt_boxes = torch.FloatTensor(1)
+
+  # ship to cuda
+  if args.cuda:
+    im_data = im_data.cuda()
+    im_info = im_info.cuda()
+    num_boxes = num_boxes.cuda()
+    gt_boxes = gt_boxes.cuda()
+
+  # make variable
+  im_data = Variable(im_data, volatile=True)
+  im_info = Variable(im_info, volatile=True)
+  num_boxes = Variable(num_boxes, volatile=True)
+  gt_boxes = Variable(gt_boxes, volatile=True)
+
+  if args.cuda:
+    cfg.CUDA = True
+
+  if args.cuda:
+    fasterRCNN.cuda()
+
+  start = time.time()
+  max_per_image = 100
+
+  # vis = args.vis
+  vis = False
+  
+  if vis:
+    thresh = 0.05
+  else:
+    thresh = 0.0
+
+  save_name = 'faster_rcnn_10'
+  num_images = len(imdb.image_index)
+  all_boxes = [[[] for _ in xrange(num_images)]
+               for _ in xrange(imdb.num_classes)]
+
+  output_dir = get_output_dir(imdb, save_name)
+  dataset = roibatchLoader(roidb, ratio_list, ratio_index, args.batch_size, \
+                        imdb.num_classes, training=False, normalize = False)
+  dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size,
+                            shuffle=False, num_workers=0,
+                            pin_memory=True)
+
+  data_iter = iter(dataloader)
+
+  _t = {'im_detect': time.time(), 'misc': time.time()}
+  det_file = os.path.join(output_dir, 'detections.pkl')
+
+  fasterRCNN.eval()
+  empty_array = np.transpose(np.array([[],[],[],[],[]]), (1,0))
+  for i in range(num_images):
+
+      data = next(data_iter)
+      im_data.data.resize_(data[0].size()).copy_(data[0])
+      im_info.data.resize_(data[1].size()).copy_(data[1])
+      gt_boxes.data.resize_(data[2].size()).copy_(data[2])
+      num_boxes.data.resize_(data[3].size()).copy_(data[3])
+
+      det_tic = time.time()
+      pooled_feat = fasterRCNN(im_data, im_info, gt_boxes, num_boxes)
+
+      single_feat = pooled_feat.data[0, 0, :, :]
+      print(single_feat.size())
+      # boxes = rois.data[:, :, 1:5]
+
+
+      misc_toc = time.time()
+      nms_time = misc_toc - misc_tic
+
+      sys.stdout.write('im_detect: {:d}/{:d} {:.3f}s {:.3f}s   \r' \
+          .format(i + 1, num_images, detect_time, nms_time))
+      sys.stdout.flush()
+
+      if vis:
+          img_save_file = os.path.join(image_save_dir, "{}.png".format(i))
+          cv2.imwrite(img_save_file, im2show)
+          #cv2.imshow('test', im2show)
+          #cv2.waitKey(0)
+
+  end = time.time()
+  print("test time: %0.4fs" % (end - start))
