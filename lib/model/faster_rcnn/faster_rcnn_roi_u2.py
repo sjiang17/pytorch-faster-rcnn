b4d7c10969d53b484b31e81b733f118ceffeef6d
sjiang17 <styjiang@163.com>
2018-06-07 17:32:31 -0700

    fixed indent

--- lib/model/faster_rcnn/faster_rcnn_roi_u2.py
+++ lib/model/faster_rcnn/faster_rcnn_roi_u2.py
@@ -43,30 +43,30 @@ class _fasterRCNN(nn.Module):
         lrelu = nn.LeakyReLU(0.1, True)
 
         e1_conv = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=use_bias)
-		e1_norm = nn.BatchNorm2d(512)
-		self.e1 = nn.Sequential(e1_conv, e1_norm, lrelu)
+        e1_norm = nn.BatchNorm2d(512)
+        self.e1 = nn.Sequential(e1_conv, e1_norm, lrelu)
 
-		e2_conv = nn.Conv2d(512, 1024, kernel_size=4, stride=2, padding=1, bias=use_bias)
-		e2_norm = nn.BatchNorm2d(1024) 
-		self.e2 = nn.Sequential(e2_conv, e2_norm, lrelu)
-		
-		self.d1_deconv = nn.ConvTranspose2d(1024, 512, kernel_size=4, stride=2, padding=1, bias=use_bias)
-		d1_norm = nn.BatchNorm2d(512)
-		if use_dropout:
-			self.d1 = nn.Sequential(d1_norm, nn.Dropout(0.5), lrelu)
-		else:
-			self.d1 = nn.Sequential(d1_norm, lrelu)
-		
-		d2_conv = nn.Conv2d(1024, 512, kernel_size=3, stride=1, padding=1, bias=use_bias)
-		d2_norm = nn.BatchNorm2d(512)
-		if use_dropout:
-			self.d2 = nn.Sequential(d2_conv, d2_norm, nn.Dropout(0.5), lrelu)
-		else:
-			self.d2 = nn.Sequential(d2_conv, d2_norm, lrelu)
+        e2_conv = nn.Conv2d(512, 1024, kernel_size=4, stride=2, padding=1, bias=use_bias)
+        e2_norm = nn.BatchNorm2d(1024) 
+        self.e2 = nn.Sequential(e2_conv, e2_norm, lrelu)
 
-		d3_conv = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=use_bias)
-		d3_relu = nn.ReLU(True)
-		self.d3 = nn.Sequential(d3_conv, d3_relu)
+        self.d1_deconv = nn.ConvTranspose2d(1024, 512, kernel_size=4, stride=2, padding=1, bias=use_bias)
+        d1_norm = nn.BatchNorm2d(512)
+        if use_dropout:
+            self.d1 = nn.Sequential(d1_norm, nn.Dropout(0.5), lrelu)
+        else:
+            self.d1 = nn.Sequential(d1_norm, lrelu)
+
+        d2_conv = nn.Conv2d(1024, 512, kernel_size=3, stride=1, padding=1, bias=use_bias)
+        d2_norm = nn.BatchNorm2d(512)
+        if use_dropout:
+            self.d2 = nn.Sequential(d2_conv, d2_norm, nn.Dropout(0.5), lrelu)
+        else:
+            self.d2 = nn.Sequential(d2_conv, d2_norm, lrelu)
+
+        d3_conv = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=use_bias)
+        d3_relu = nn.ReLU(True)
+        self.d3 = nn.Sequential(d3_conv, d3_relu)
         #####################
 
     def forward(self, im_data, im_info, gt_boxes, num_boxes):
@@ -118,12 +118,12 @@ class _fasterRCNN(nn.Module):
 
         #################
         x_input = pooled_feat
-		x = self.e1(x_input)
-		x = self.e2(x)
-		x = self.d1_deconv(x, output_size=x_input.size())
-		x = self.d1(x)
-		x = self.d2(torch.cat([x_input, x], 1))
-		x = self.d3(x)
+        x = self.e1(x_input)
+        x = self.e2(x)
+        x = self.d1_deconv(x, output_size=x_input.size())
+        x = self.d1(x)
+        x = self.d2(torch.cat([x_input, x], 1))
+        x = self.d3(x)
         #################
 
         # feed pooled features to top model
